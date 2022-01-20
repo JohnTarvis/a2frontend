cbd91ba7e7d6aa2ab7b63525c30b5c718635606b
John Saville <johnasaville@gmail.com>
2022-01-20 05:21:52 -0500

    Update a2api.js

--- src/api/a2api.js
+++ src/api/a2api.js
@@ -3,6 +3,7 @@ import { response } from 'express';
 
 const app = require('express');
 const cors = require('cors');
+app.use(response);
 app.use(cors);
 
 const BASE_URL = 'https://damp-island-15072.herokuapp.com';//"http://localhost:3001";
