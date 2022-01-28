7e1b7044746e9526fd25ca04ced657a18646d706
John Saville <johnasaville@gmail.com>
2022-01-28 15:52:57 -0500

    Update NewPostForm.js

--- src/posts/NewPostForm.js
+++ src/posts/NewPostForm.js
@@ -63,8 +63,8 @@ function NewPostForm({ createPost }) {
 
   const S3_BUCKET ='a2uploads';
   const REGION ='us-west-1';
-  const ACCESS_KEY = +process.env.S3_ACCESS_KEY_ID;
-  const SECRET_ACCESS_KEY = +process.env.S3_SECRET_ACCESS_KEY;
+  const ACCESS_KEY = process.env.S3_ACCESS_KEY_ID;
+  const SECRET_ACCESS_KEY = process.env.S3_SECRET_ACCESS_KEY;
 
   const config = {
     bucketName: S3_BUCKET,
