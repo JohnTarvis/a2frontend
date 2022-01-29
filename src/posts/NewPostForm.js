227673f96eb7ac6da8a849029bf6f37c7048bdd1
John Saville <johnasaville@gmail.com>
2022-01-29 18:41:25 -0500

    Update NewPostForm.js

--- src/posts/NewPostForm.js
+++ src/posts/NewPostForm.js
@@ -43,8 +43,8 @@ function NewPostForm({ createPost }) {
     const date = new Date();
     formData.post_date = date;
 
-    console.log('selectedFile====================================================',selectedFile);
-    console.log('filename========================================================',selectedFile.name);
+    // console.log('selectedFile====================================================',selectedFile);
+    // console.log('filename========================================================',selectedFile.name);
 
     formData.image = selectedFile.name;
     formData.file = selectedFile;
