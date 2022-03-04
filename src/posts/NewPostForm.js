8acd87427b82b408c4a05d520cb0ceee0b58dcd7
John Saville <johnasaville@gmail.com>
2022-03-03 21:47:34 -0500

    Update NewPostForm.js

--- src/posts/NewPostForm.js
+++ src/posts/NewPostForm.js
@@ -49,8 +49,10 @@ function NewPostForm({ createPost }) {
 
     console.log('file|||||||||||||||||||||||||||||||',selectedFile);
 
-    formData.file = selectedFile;
+    // formData.file = selectedFile;
     // formData.append('file',selectedFile);
+
+    formData.append('file',selectedFile);
 
 
     formData.image = `https://a2uploads.s3.us-west-1.amazonaws.com/${selectedFile.name}`;
