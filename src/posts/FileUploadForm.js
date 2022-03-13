dad90bd477709c54e051adfc0cb7739fbcf6255d
John Saville <johnasaville@gmail.com>
2022-03-12 21:57:06 -0500

    Update FileUploadForm.js

--- src/posts/FileUploadForm.js
+++ src/posts/FileUploadForm.js
@@ -1,13 +1,33 @@
+// import React, { Component } from 'react';
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const backendURL = 'https://damp-island-15072.herokuapp.com/post';
 
 function FileUploadForm(){
+
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    function onFileChange(e) {
+        setSelectedFile(e.target.files[0])
+    }
+    function onSubmit(e) {
+        e.preventDefault()
+        const formData = new FormData()
+        formData.append('profileImg', selectedFile)
+        axios.post(backendURL, formData, {
+        }).then(res => {
+            console.log(res)
+            console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+        })
+    }
+
     return (
         <div className="container">
             <div className="row">
-                <form>
-                    <h3>React File Upload</h3>
+                <form onSubmit={onSubmit}>
                     <div className="form-group">
-                        <input type="file" />
+                        <input type="file" onChange={onFileChange} />
                     </div>
                     <div className="form-group">
                         <button className="btn btn-primary" type="submit">Upload</button>
@@ -16,6 +36,48 @@ function FileUploadForm(){
             </div>
         </div>
     );
+    
 }
+
+
+// export default class FilesUploadComponent extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.onFileChange = this.onFileChange.bind(this);
+//         this.onSubmit = this.onSubmit.bind(this);
+//         this.state = {
+//             profileImg: ''
+//         }
+//     }
+//     onFileChange(e) {
+//         this.setState({ profileImg: e.target.files[0] })
+//     }
+//     onSubmit(e) {
+//         e.preventDefault()
+//         const formData = new FormData()
+//         formData.append('profileImg', this.state.profileImg)
+//         axios.post("http://localhost:4000/api/user-profile", formData, {
+//         }).then(res => {
+//             console.log(res)
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div className="container">
+//                 <div className="row">
+//                     <form onSubmit={this.onSubmit}>
+//                         <div className="form-group">
+//                             <input type="file" onChange={this.onFileChange} />
+//                         </div>
+//                         <div className="form-group">
+//                             <button className="btn btn-primary" type="submit">Upload</button>
+//                         </div>
+//                     </form>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
 export default FileUploadForm;
