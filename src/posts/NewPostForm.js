4a18c0b2305a4de3da6f3cbf56f97e880b4084c3
John Saville <johnasaville@gmail.com>
2022-03-13 19:00:44 -0400

    Update NewPostForm.js

--- src/posts/NewPostForm.js
+++ src/posts/NewPostForm.js
@@ -64,12 +64,15 @@ function NewPostForm({ createPost,testPost }) {
 
     const formData2 = new FormData();
 
+    // date = false;
+
     formData2.append('upload',selectedFile);
     formData2.append('post_body',formData.post_body);
     formData2.append('post_subject',formData.post_subject);
     formData2.append('post_tags',formData.post_tags);
     formData2.append('image',formData.image);
-    formData2.append('post_date',date);
+
+    // formData2.append('post_date',date);
 
 
     console.log('formData2================================================',formData2);
