3d4561cd2d387289415f1b74408b351fbda8543c
John Saville <53898479+JohnTarvis@users.noreply.github.com>
2022-03-26 19:38:23 -0600

    Update PostsDisplay.js

--- src/threads/PostsDisplay.js
+++ src/threads/PostsDisplay.js
@@ -50,7 +50,7 @@ function PostsDisplay(){
         maxJumps--;
         elem = elem.parentNode;
       }
-      const postId = elem.id.slice(9);
+      const postId = elem.id.slice(12);
       console.log('postid=================================',postId);
       history.push(`/posts/${postId}`);
     }    
