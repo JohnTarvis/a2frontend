c101be80be89d7f6404d96c7ec27945462243546
John Saville <53898479+JohnTarvis@users.noreply.github.com>
2022-04-15 21:30:30 -0600

    Update ThreadsDisplay.js

--- src/threads/ThreadsDisplay.js
+++ src/threads/ThreadsDisplay.js
@@ -4,12 +4,24 @@ import A2Api from "../api/a2api";
 import LoadingSpinner from "../common/LoadingSpinner";
 import ThreadPreview from "./ThreadPreview";
 import { useHistory } from "react-router-dom";
+// import '../styles/ThreadsDisplay.css';
 import TopBanner from "../top-banner/TopBanner";
+
 import PostContext from "./PostContext";
+
 import A2api2 from "../api/a2api2";
 
-const history = useHistory();
+function randomBG(){
+  let L = () => Math.ceil(Math.random() * 8);
+  return `#${L()}${L()}${L()}`;
 
+}
+
+const currentBG = randomBG();
+
+const style = {
+  backgroundColor: currentBG,
+}
 
 
 
@@ -27,30 +39,33 @@ async function ThreadsDisplay(){
       setAllPosts(nonReplies);
     }
 
+    const history = useHistory();
+
     if (!allPosts) return <LoadingSpinner />;
- 
+
     async function clickHandler(e){
       let elem = e.target;
       let maxJumps = 99;
       let pressed = false;
-    
+
       if(elem.className == 'delete-thread-button'){
         pressed = true;
       }
-    
+
       while(elem.className != 'ThreadPreview' && maxJumps > 0){
         maxJumps--;
         elem = elem.parentNode;
       }
       const postId = elem.id.slice(14);
-    
+
       if(pressed){
         console.log('postid in threadsdisplay',postId);
         await A2Api.deleteThread(postId);
       } else {
         history.push(`/posts/${postId}`);
       }
-    }   
+    }    
+
 
     return (
       <div className="main-view" >
